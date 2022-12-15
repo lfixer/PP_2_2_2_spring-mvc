239927732948437dcf1d94b391fa6ea5da5bcd2c
lfixer <strmgddn@gmail.com>
2022-12-15 23:05:32 +0300

    Исправления

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImp;
 
 import java.util.List;
@@ -12,10 +14,16 @@ import java.util.List;
 @Controller
 public class CarsController {
 
+    private CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
-        CarServiceImp carsServiceImpl = new CarServiceImp();
-        List<Car> qtyCars = carsServiceImpl.getCars(count);
+        List<Car> qtyCars = carService.getCars(count);
         model.addAttribute("cars", qtyCars);
         return "cars";
     }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,10 +1,12 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImp implements CarService {
 
     private ArrayList<Car> cars = new ArrayList<>();
